c7a6c5525db6698bd0ba771b4171ce576ea9a892
Rick <rickpowell1311@gmail.com>
2017-03-03 11:41:46 +0000

    Specifying nuspec for Mapping project

--- src/SimpleMapping/Properties/AssemblyInfo.cs
+++ src/SimpleMapping/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RickPowell.Mapping")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SimpleMapping")]
+[assembly: AssemblyDescription("A simple mapping framework for mapping to or populating from objects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RickPowell.Mapping")]
+[assembly: AssemblyCompany("None")]
+[assembly: AssemblyProduct("SimpleMapping")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
